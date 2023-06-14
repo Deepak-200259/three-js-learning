a6ef02b7733838ec3b8b4230f60f26888ef0e91c
Deepak-Chicmic <deepak.saini@chicmic.co.in>
2023-06-14 14:19:35 +0530

    First React App Finsihed

--- 41_FirstReactApp/src/people.jsx
+++ 41_FirstReactApp/src/people.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 export default function People() {
+  // const [people, setPeople] = useState([
+  //   { id: 1, name: "John" },
+  //   { id: 2, name: "Jane" },
+  //   { id: 3, name: "Sudo" },
+  //   { id: 4, name: "Boy" },
+  // ]);
+  const [people, setPeople] = useState([]);
+
+  // const getPoeple = () => {
+  //   const request = fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((result) => console.log(result));
+  // };
+  const getPoeple = async () => {
+    const request = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await request.json();
+    setPeople(response);
+  };
+
+  useEffect(() => {
+    getPoeple();
+  }, []);
   return (
     <div>
       <h2>People</h2>
+      <ul>
+        {people.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
